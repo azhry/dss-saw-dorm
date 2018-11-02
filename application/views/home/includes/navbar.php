46f5249d12ff2920f27e990278b430a9ec2c127f
azhry <arliansyah_azhary@yahoo.com>
2018-11-02 22:52:59 +0700

    first commit

---
+++ application/views/home/includes/navbar.php
@@ -0,0 +1,17 @@
+<!-- top navigation -->
+<div class="top_nav">
+    <div class="nav_menu">
+        <nav>
+            <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+            </div>
+            <ul class="nav navbar-nav navbar-right">
+                <li>
+                    <a href="<?= base_url('login') ?>">Login</a>
+                </li>
+                <li><a href="<?= base_url() ?>"><h5>Sistem Sewa Ruko</h5></a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+<!-- /top navigation -->
---
+++ application/views/home/includes/sidebar.php
@@ -0,0 +1,23 @@
+<div class="container body">
+    <div class="main_container">
+        <div class="col-md-3 left_col">
+            <div class="left_col scroll-view">
+                <div class="navbar nav_title" style="border: 0;">
+                    <a href="<?= base_url('') ?>" class="site_title"><i class="fa fa-paw"></i> <span>Sewa Ruko</span></a>
+                </div>
+                <div class="clearfix"></div>
+                <br />
+                <!-- sidebar menu -->
+                <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+                    <div class="menu_section">
+                        <h3>Menu</h3>
+                        <ul class="nav side-menu">
+                            <li><a href="<?= base_url('home') ?>"><i class="fa fa-table"></i> Daftar Ruko</a></li>
+                            <li><a href="<?= base_url('home/cari') ?>"><i class="fa fa-search"></i> Cari Ruko</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /sidebar menu -->
+    </div>
